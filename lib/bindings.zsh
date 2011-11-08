f0e3d130f23c45884a2b490edc5a53af34d8381c
Caius Durling <dev@caius.name>
2011-11-08 19:51:49 +0000

    Make key bindings work wot how I expect

--- lib/bindings.zsh
+++ lib/bindings.zsh
@@ -7,8 +7,8 @@ bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
-# bindkey '^[[A' up-line-or-search
-# bindkey '^[[B' down-line-or-search
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
@@ -18,3 +18,8 @@ bindkey ' ' magic-space    # also do history expansion on space
 
 ## file rename magick
 bindkey "^[m" copy-prev-shell-word
+
+# "Fix" delete
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "^?" backward-delete-char
