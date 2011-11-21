2bd93ca8e057a631716a475db79978dbee4557ea
Caius Durling <dev@caius.name>
2011-11-21 15:04:22 +0000

    Case _in_ sensitive completion

--- lib/1-main.zsh
+++ lib/1-main.zsh
@@ -27,7 +27,8 @@ WORDCHARS=''
 
 zmodload -i zsh/complist
 
-zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# Case insensitive matching
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
 
 unsetopt MENU_COMPLETE
