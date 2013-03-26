18d9df8468ba831e2572ec1afc200f2813111b2b
Caius Durling <dev@caius.name>
2013-03-26 13:02:21 +0000

    Some 8 cores, some 4 cores

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -31,7 +31,7 @@ export EDITOR='mate'
 export PYTHONPATH="/usr/local/lib/python2.6/site-packages"
 
 # Fuck you make
-export MAKEFLAGS='-j4'
+export MAKEFLAGS="-j$(sysctl machdep.cpu.thread_count | awk '{ print $2 }')"
 
 # From http://grease-your-suite.heroku.com/#87
 export RUBY_HEAP_MIN_SLOTS=1000000
@@ -39,6 +39,7 @@ export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
+export RUBY_FREE_MIN=200000
 
 # Coz I wanna be different, yeah?
 export SUDO_PROMPT="Sudo Password: "
