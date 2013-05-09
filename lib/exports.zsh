31b3de023b65b21001626b0798fb662be29c504f
Caius Durling <dev@caius.name>
2013-05-09 18:11:45 +0100

    Java go home

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -34,6 +34,9 @@ export PYTHONPATH="/usr/local/lib/python2.7/site-packages"
 # Fuck you make
 export MAKEFLAGS="-j$(sysctl machdep.cpu.thread_count | awk '{ print $2 }')"
 
+# Fuck you JAVA
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.6)"
+
 # From http://grease-your-suite.heroku.com/#87
 export RUBY_HEAP_MIN_SLOTS=1000000
 export RUBY_HEAP_SLOTS_INCREMENT=1000000
