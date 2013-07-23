70ce2ecd50418ea813fc6939824b39c0523f9b3d
Caius Durling <dev@caius.name>
2013-07-23 11:03:37 +0100

    Pin rbenv version

--- lib/rbenv.zsh
+++ lib/rbenv.zsh
@@ -1,4 +1,6 @@
 # Use homebrew's folders rather than ~/.rbenv
 export RBENV_ROOT=/usr/local/var/rbenv
+# Default to this version of 1.9.3
+export RBENV_VERSION=1.9.3-p327-perf
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
