e3eb4d6b9d954dbc98e8efa8c55fb9a26148fdef
Caius Durling <dev@caius.name>
2013-08-29 10:59:41 +0100

    Swap out chruby for rbenv

---
+++ lib/chruby.zsh
@@ -0,0 +1,8 @@
+if [[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
+
+  # Load up chruby
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+  # Auto change rubies
+  source /usr/local/opt/chruby/share/chruby/auto.sh
+
+fi
--- lib/rbenv.zsh
+++
@@ -1,6 +0,0 @@
-if which rbenv > /dev/null; then
-  # Use homebrew's folders rather than ~/.rbenv
-  export RBENV_ROOT=/usr/local/var/rbenv
-
-  eval "$(rbenv init -)"
-fi
