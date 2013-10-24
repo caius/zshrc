62c4ab81bfcafdc8b679384bef5c430852102722
Caius Durling <dev@caius.name>
2013-10-24 23:14:59 +0100

    Ignore OS X java setup on FreeBSD

--- lib/java.zsh
+++ lib/java.zsh
@@ -1,9 +1,12 @@
-if [[ "13.0.0" == "$(uname -r)" ]]; then
-	# Mavericks, psh
-	JAVA_VERSION="1.7"
-else
-	JAVA_VERSION="1.6"
-fi
+if [[ -f /usr/libexec/java_home ]]; then
 
-# Fuck you JAVA
-export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+  if [[ "13.0.0" == "$(uname -r)" ]]; then
+	  # Mavericks, psh
+	  JAVA_VERSION="1.7"
+  else
+	  JAVA_VERSION="1.6"
+  fi
+
+  # Fuck you JAVA
+  export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+fi
