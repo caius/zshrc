1de92fead7340140577d5e773f11185d57231b9b
Caius Durling <dev@caius.name>
2014-02-07 14:57:56 +0000

    Revert "No longer require per-project rubies"
    This reverts commit 743baf952dd5eb56906305a986391ebdbd78a70f.

--- lib/chruby.zsh
+++ lib/chruby.zsh
@@ -3,9 +3,6 @@ if [[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
   # Load up chruby
   source /usr/local/opt/chruby/share/chruby/chruby.sh
   # Auto change rubies
-  # source /usr/local/opt/chruby/share/chruby/auto.sh
-
-  # Load default ruby for entire shell session
-  test -f ~/.ruby-version && chruby $(< ~/.ruby-version)
+  source /usr/local/opt/chruby/share/chruby/auto.sh
 
 fi
