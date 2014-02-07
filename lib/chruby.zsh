743baf952dd5eb56906305a986391ebdbd78a70f
Caius Durling <dev@caius.name>
2014-02-07 13:18:56 +0000

    No longer require per-project rubies

--- lib/chruby.zsh
+++ lib/chruby.zsh
@@ -3,6 +3,9 @@ if [[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
   # Load up chruby
   source /usr/local/opt/chruby/share/chruby/chruby.sh
   # Auto change rubies
-  source /usr/local/opt/chruby/share/chruby/auto.sh
+  # source /usr/local/opt/chruby/share/chruby/auto.sh
+
+  # Load default ruby for entire shell session
+  test -f ~/.ruby-version && chruby $(< ~/.ruby-version)
 
 fi
