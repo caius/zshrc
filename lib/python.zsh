dce39b2fa02ec54bf031ae6123d4eba846e7fd0f
Caius Durling <dev@caius.name>
2014-02-16 00:15:48 +0000

    Setup python in my environment

---
+++ lib/python.zsh
@@ -0,0 +1,5 @@
+# Python (homebrewed) settings
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV="true"
+# cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
