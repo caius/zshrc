2528a49572171ce6adf58d3dfcbdc8b5a07de381
Caius Durling <dev@caius.name>
2014-03-04 18:33:46 +0000

    Be smarter about setting $JAVA_HOME

--- lib/java.zsh
+++ lib/java.zsh
@@ -1,12 +1,5 @@
 if [[ -f /usr/libexec/java_home ]]; then
-
-  if [[ "13.0.0" == "$(uname -r)" ]]; then
-	  # Mavericks, psh
-	  JAVA_VERSION="1.7"
-  else
-	  JAVA_VERSION="1.6"
-  fi
-
   # Fuck you JAVA
+  JAVA_VERSION=$(/usr/libexec/java_home -V 2>&1 | awk '/1\./ { gsub(",", "", $1); print $1; exit }')
   export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
 fi
