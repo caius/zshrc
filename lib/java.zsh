9bfb985b26a3f35a497b1347f1a7f13cc0be0fb8
Caius Durling <dev@caius.name>
2014-06-09 08:42:26 -0700

    Handle OS X with no java installed

--- lib/java.zsh
+++ lib/java.zsh
@@ -1,5 +1,7 @@
 if [[ -f /usr/libexec/java_home ]]; then
   # Fuck you JAVA
   JAVA_VERSION=$(/usr/libexec/java_home -V 2>&1 | awk '/1\./ { gsub(",", "", $1); print $1; exit }')
-  export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+  if [[ ! -z $JAVA_VERSION ]]; then
+    export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+  fi
 fi
