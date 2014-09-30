1aa37338e0c0dfc45cf95d88e6e2afd3236d2892
Caius Durling <dev@caius.name>
2014-09-30 10:42:55 +0100

    Bump up ZSH history size

--- lib/history.zsh
+++ lib/history.zsh
@@ -1,7 +1,7 @@
 ## Command history configuration
 HISTFILE=$HOME/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 
 setopt hist_ignore_dups # ignore duplication command history list
 setopt share_history # share command history data
