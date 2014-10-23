ae2f59e3ec7a34ec6b29c4b5742db04441efc1c3
Caius Durling <dev@caius.name>
2014-10-23 09:20:59 +0100

    Simplify JAVA_HOME finding logic

--- lib/java.zsh
+++ lib/java.zsh
@@ -1,7 +1,3 @@
 if [[ -f /usr/libexec/java_home ]]; then
-  # Fuck you JAVA
-  JAVA_VERSION=$(/usr/libexec/java_home -V 2>&1 | awk '/1\./ { gsub(",", "", $1); print $1; exit }')
-  if [[ ! -z $JAVA_VERSION ]]; then
-    export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
-  fi
+  export JAVA_HOME="$(/usr/libexec/java_home -V 2>&1 | awk 'NR == 2 { print $6 }')"
 fi
