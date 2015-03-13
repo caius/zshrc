b42a5755056cb02949673066a7ac0929d5a14327
Caius Durling <dev@caius.name>
2015-03-13 10:08:29 +0000

    Use almost all CPUs for parallel things

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -38,9 +38,9 @@ export LC_CTYPE=en_US.UTF-8 LANG=en_GB.UTF-8
 export EDITOR="mate -wl1"
 export SVN_EDITOR="$EDITOR" HGEDITOR="$EDITOR" GIT_EDITOR="$EDITOR"
 
-# Fuck you make
-cpus="$(/usr/sbin/sysctl -n machdep.cpu.thread_count)"
-export MAKEFLAGS="-j${cpus}" BUNDLER_JOBS="${cpus}"
+# Fuck you make, et al. Leave some CPU for the rest of us.
+parallel_cpus=$(( $(/usr/sbin/sysctl -n machdep.cpu.thread_count) - 1 ))
+export MAKEFLAGS="-j${parallel_cpus}" BUNDLER_JOBS="${parallel_cpus}" WORKER_COUNT="${parallel_cpus}"
 
 # Coz I wanna be different, yeah?
 export SUDO_PROMPT="Sudo Password: "
