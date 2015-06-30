e9724a1ccdb1543bc269e840cd25518be50fee4a
Caius Durling <dev@caius.name>
2015-06-30 11:40:49 +0100

    Add notes on ulimit tweaks

--- lib/ulimit.zsh
+++ lib/ulimit.zsh
@@ -1,2 +1,9 @@
+# For this to work you'll want to put the following into /etc/sysctl.conf:
+#
+#   # Bump the open files limit
+#   kern.maxfiles=262144
+#   kern.maxfilesperproc=32768
+#
+
 # Thanks OS X
 ulimit -n 32768
