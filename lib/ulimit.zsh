8c9255e6640f9dba1033076a1d2e73a1cbe89527
Caius Durling <dev@caius.name>
2015-06-30 12:45:42 +0100

    Guard OS X setting with a check

--- lib/ulimit.zsh
+++ lib/ulimit.zsh
@@ -1,9 +1,12 @@
-# For this to work you'll want to put the following into /etc/sysctl.conf:
-#
-#   # Bump the open files limit
-#   kern.maxfiles=262144
-#   kern.maxfilesperproc=32768
-#
+if [[ "$(uname)" == "Darwin" ]]; then
 
-# Thanks OS X
-ulimit -n 32768
+  # For this to work you'll want to put the following into /etc/sysctl.conf:
+  #
+  #   # Bump the open files limit
+  #   kern.maxfiles=262144
+  #   kern.maxfilesperproc=32768
+  #
+
+  # Thanks OS X
+  ulimit -n 32768
+fi
