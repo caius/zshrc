9064259268688193cca119d6197f408f906ba44f
Caius Durling <dev@caius.name>
2016-06-15 17:12:03 +0100

    Expand & squeeze slashes please

--- lib/1-main.zsh
+++ lib/1-main.zsh
@@ -30,6 +30,8 @@ zmodload -i zsh/complist
 # Case insensitive matching
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' expand 'yes'
+zstyle ':completion:*' squeeze-slashes 'yes'
 
 unsetopt MENU_COMPLETE
 
