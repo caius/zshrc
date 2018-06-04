beccbcbb0d38cabceafd0024f7d04bb5a681e1bf
Caius Durling <dev@caius.name>
2018-06-04 12:22:26 +0100

    Add some homebrew security flags

--- lib/homebrew.zsh
+++ lib/homebrew.zsh
@@ -3,4 +3,11 @@
 # Don't search github for me, I can do that if I care
 export HOMEBREW_NO_GITHUB_API=1
 
+# Don't auto-update, I can do that by hand when I want to
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+# Don't follow insecure redirects
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+# Make sure casks have SHAs for downloaded packages when installing
+export HOMEBREW_CASK_OPTS=--require-sha
