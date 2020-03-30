78904bb1a65031ac15d6c927cb03eeb654ec53e9
Caius Durling <dev@caius.name>
2020-03-30 19:09:18 +0100

    Fuzzy the fuzzy search

--- lib/zzz-zsh-history-substring-search.zsh
+++ lib/zzz-zsh-history-substring-search.zsh
@@ -1,3 +1,6 @@
 source "$ZSH_ROOT/vendor/zsh-history-substring-search/zsh-history-substring-search.zsh"
 
+# Fuzzy search "curl http" matches "curl -vI http://caius.name/"
+export HISTORY_SUBSTRING_SEARCH_FUZZY=1
+
 bindkey '^R' history-substring-search-up
