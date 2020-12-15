d29a7953f29ae772d3231a690b4ccb90f9673849
Caius Durling <dev@caius.name>
2020-12-15 22:52:38 +0000

    Netlify

---
+++ lib/netlify.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for Netlify's Git Credential Helper.
+if [[ -f '/Users/caius/.netlify/helper/path.zsh.inc' ]]
+then
+  source '/Users/caius/.netlify/helper/path.zsh.inc'
+fi
